7ba16faad81a5a5bee1dd801fea3acf96900aba2
m-Shehzaib <98905825+muhammadshehzaib@users.noreply.github.com>
2023-12-22 12:38:34 +0500

    route added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("./config/db");
 // const api = ;
-const PORT = 4000;
+const PORT = 4001;
 const app = express();
 const bookModel = require("./model/book_model");
 require("dotenv").config();
