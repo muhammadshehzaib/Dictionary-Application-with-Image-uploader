be2358c630bee4c16b1e68ff3f9bce289e84c0ee
m-Shehzaib <98905825+muhammadshehzaib@users.noreply.github.com>
2023-12-22 14:06:09 +0500

    changes

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ const app = express();
 const bookModel = require("./model/book_model");
 require("dotenv").config();
 
-app.use(api);
 app.use(express.json());
+app.use(api);
+
+app.get("/", (req, res) => {
+  res.send("Main app.js");
+});
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running at " + process.env.PORT);
