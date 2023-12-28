4340c29059277138e4ffe89d993360f464b7dffd
m-Shehzaib <98905825+muhammadshehzaib@users.noreply.github.com>
2023-12-28 12:55:07 +0500

    backend deployed vercel

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("./config/db");
-app.use(cors());
 const router = require("./routes/api");
 const UserRouter = require("./routes/User_Routes");
 require("dotenv").config();
@@ -10,7 +9,13 @@ const PORT = 4000;
 
 app.use(express.json());
 require("dotenv").config();
-
+const corsOptions = {
+  origin: "https://todo-backend-bxlaemgc2-muhammadshehzaib.vercel.app",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
 app.use(router);
 app.use(UserRouter);
 
