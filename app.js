be7e6645931e4b72b853e5755cb482fc79b60fd7
m-Shehzaib <98905825+muhammadshehzaib@users.noreply.github.com>
2023-12-28 15:49:44 +0500

    backend deployed vercel

--- app.js
+++ app.js
@@ -18,12 +18,7 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  const allowedOrigins = ["https://todo-backend-umber-xi.vercel.app"];
-  const origin = req.headers.origin;
-
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
